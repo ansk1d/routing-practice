577ca5ca1c99d6405ab54b00d30398f8b240235e
ansk1d <anasalkhaled@aol.com>
2022-08-28 14:30:10 +0300

    edit

--- src/components/TxtColored.jsx
+++ src/components/TxtColored.jsx
@@ -4,7 +4,13 @@ import { useParams } from "react-router";
 const TxtColored = (props) => {
     const { word,backgroundColor,color } = useParams();
     return (
-        <h1 style={{backgroundColor:backgroundColor,color:color}}>The word is {word}</h1>
-        );
+        <div>
+            {
+                isNaN(word)?
+                <h1 style={{backgroundColor:backgroundColor,color:color}}>The Word is {word}</h1>:
+                <h1 style={{backgroundColor:backgroundColor,color:color}}>The Number is {word}</h1>
+            }
+        </div>
+    )
     }
     export default TxtColored;
